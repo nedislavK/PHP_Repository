baa52f7229c9cb1eac15a24b84ae08cdd3ecce88
Nedislav <nedislav1998@gmail.com>
2020-10-16 08:58:35 +0300

    upr3

--- login.php
+++ login.php
@@ -1,21 +1,46 @@
 <?php
 
-$legitUsername = "nudes";
-$legitPassword = "69";
+//$legitUsername = "nudes";
+//$legitPassword = "69";
 
-print_r($_POST);
-function login()
-{
-    $legitUsername = "nudes";
-    $legitPassword = "69";
 
-    if ($_POST["username"] == $legitUsername &&
-        $_POST["password"] == $legitPassword
-    ) {
+
+function getUserData() {
+    $servername = "localhost";
+    $db = "example";
+    $db_username = "root";
+    $db_password = "";
+    $connection;
+    try{
+        $connection = new PDO(
+            "mysql:host=$servername;dbname=$db",
+            $db_username,
+            $db_password
+        );
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+    $sql = "SELECT * FROM user_credentials WHERE username = '".$_POST['username']."' AND password = '".$_POST['password']."'";
+    echo $sql;
+    $result = $connection->query($sql);
+    $userData = $result->fetchAll();
+    if (empty($userData)) {
+        return [];
+    } else {
+        return $userData[0];
+    }
+}
+
+
+function login() {
+
+    if (!empty(getUserData())) {
         echo "Logged in congrats";
         header("Location: welcome.html");
     } else {
         header("Location: index.html");
     }
 }
+
 login();
